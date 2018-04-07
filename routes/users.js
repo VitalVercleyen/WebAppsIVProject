960feb73da667a8de3bebb00307963e5721ae617
Vital Vercleyen <vital.vercleyen.y2329@student.hogent.be>
2018-04-08 00:17:39 +0200

    user register

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
 
 // Registreren
-router.get("/register", (req, res, next) => {
-  res.send("REGISTER");
+router.post("/register", (req, res, next) => {
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  User.addUser(newUser, (err, user) => {
+    if (err) {
+      res.json({ succes: false, msg: "Failed to register user" });
+    } else {
+      res.json({ succes: true, msg: "User registered" });
+    }
+  });
 });
 
 // Authenticatie
@@ -14,11 +30,6 @@ router.post("/authenticate", (req, res, next) => {
 // Profiel
 router.get("/profile", (req, res, next) => {
   res.send("PROFILE");
-});
-
-// Validatie
-router.get("/validate", (req, res, next) => {
-  res.send("VALIDATE");
 });
 
 module.exports = router;
